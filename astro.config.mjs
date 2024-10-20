99c334aef8c8f2c0ebcb4fa36a9594ff41d8af8b
Tristan Vidal <tres@duck.com>
2024-10-20 23:16:21 +0000

    Refactor Astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-      port: 4321,
-      host: '127.0.0.1'
-  },
-
+//   server: {
+//       port: 4321,
+//       host: '127.0.0.1'
+//   },
   integrations: [tailwind()],
 });
