6cbc797f779e463abe1abae1d67c918304f73c4d
Tristan <tres@duck.com>
2024-11-13 21:58:30 -0500

    Update page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
-export default defineConfig({
-//   server: {
-//       port: 4321,
-//       host: '127.0.0.1'
-//   },
-  integrations: [tailwind()],
-});
+export default defineConfig({});
